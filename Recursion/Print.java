c4ee96d4360d79eccea12c67874341bc715294f3
Parth Dhavan <parthdhavan2004@gmail.com>
2024-09-01 23:51:24 +0530

    Print n to 1

---
+++ Recursion/Print.java
@@ -0,0 +1,15 @@
+//print N to 1
+public class Nto1 {
+    public static void main(String[] args) {
+        funBoth(5);
+    }
+  
+  static void funBoth(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
