13adf96af33dc5627da5c89646d7977c5b190c16
Parth Dhavan <parthdhavan2004@gmail.com>
2024-09-01 23:59:10 +0530

    Print 1 to n , n to 1, both

--- Recursion/Print.java
+++ Recursion/Print.java
@@ -13,3 +13,25 @@ public class Nto1 {
         System.out.println(n);
     }
 }
+
+//print 1 to N
+
+ static void funRev(int n) {
+        if (n == 0) {
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
+    }
+
+//print N to 1 then 1 to N
+
+ static void funBoth(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
